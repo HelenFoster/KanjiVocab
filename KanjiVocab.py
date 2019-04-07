f004885248611ddf829be97be07da7fc9fa9299f
Helen Foster <HelenFoster@users.noreply.github.com>
2019-04-07 20:41:50 +0100

    Update init for Anki 2.1

--- KanjiVocab.py
+++ KanjiVocab.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# Copyright (C) 2015,2017  Helen Foster
+# Copyright (C) 2015,2017,2019  Helen Foster
 # License: GNU AGPL, version 3 or later; http://www.gnu.org/licenses/agpl.html
 
 
@@ -8,16 +8,4 @@
 # The README might be inside depending on how you received this!
 
 
-from aqt import mw
-from aqt.utils import showInfo
-from aqt.qt import *
-
-def updateKanjiVocab():
-    import kanjivocab.run
-    reload(kanjivocab.run)
-    kanjivocab.run.updateKanjiVocab()
-    
-action = QAction("KanjiVocab...", mw)
-#action.setShortcut(_("Ctrl+K")) #this isn't good; pick a different one or leave it out
-mw.connect(action, SIGNAL("triggered()"), updateKanjiVocab)
-mw.form.menuTools.addAction(action)
+import kanjivocab
---
+++ kanjivocab/__init__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) 2015,2017,2019  Helen Foster
+# License: GNU AGPL, version 3 or later; http://www.gnu.org/licenses/agpl.html
+
+# KanjiVocab is an Anki addon which adds known vocab to a kanji deck.
+
+from aqt import mw
+from aqt.utils import showInfo
+from aqt.qt import QAction
+
+try:
+    from importlib import reload
+except:
+    pass #Python 2 has reload built-in
+
+def updateKanjiVocab():
+    from . import run
+    reload(run)
+    run.updateKanjiVocab()
+
+action = QAction("KanjiVocab...", mw)
+action.triggered.connect(updateKanjiVocab)
+mw.form.menuTools.addAction(action)
